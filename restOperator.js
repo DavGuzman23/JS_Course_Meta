02dc4a136bfbc58b02731af2e9812d782bdb6720
DavGuzman23 <gummer2310@gmail.com>
2025-03-04 14:30:27 +0100

    restOperator examples

---
+++ restOperator.js
@@ -0,0 +1,30 @@
+const top7 = [1,2,3,4,5,6]
+
+//Rest operator, destructuring
+//Hemos dividido en 3 elementos
+const [a, b, ...rest] = top7
+
+//Esto devolvera cada valor que le pasemos a la funcion multiplicado por el otro
+function addTaxToPrices(taxRate, ...itemsBought) {
+    return itemsBought.map(item => taxRate *item)
+}
+
+//El primer valor es el primer argumento de la funcion, el resto el segundo argumento. En el segundo argumento puedo añadir tantos como quiera
+let shoppingCart = addTaxToPrices(1.1, 46, 89, 35, 79)
+console.log(shoppingCart)
+
+
+const meal = ["soup", "steak", "ice cream"]
+let [ starter2] = meal;
+console.log( starter2);
+
+let obj = {
+    key: 1,
+    value: 4
+};
+
+let output = { ...obj };
+output.value -= obj.key;
+
+console.log(output.value);
+
